defff597177aace620dd308537d6a8b8ed6df947
Zheng Wang <ax_pokl@sina.com>
2026-05-02 17:33:37 +0800

    H35

---
+++ all_results_append.bat
@@ -0,0 +1,2 @@
+for /L %%i in (22,1,24) do @(echo ===== diandeng10_b_faster2_png_H%%i.exe ===== & ".\diandeng10_b_faster2_png_H%%i.exe" & echo.) >> all_results_b.txt 2>&1
+for /L %%i in (22,1,24) do @(echo ===== diandeng10_a_faster2_png_H%%i.exe ===== & ".\diandeng10_a_faster2_png_H%%i.exe" & echo.) >> all_results_a.txt 2>&1
