1e82de2c0cc1fea4fa9b00f7eb46d97aca71fde0
Zheng Wang <ax_pokl@sina.com>
2026-05-04 22:14:50 +0800

    png

--- png_compress.bat
+++ png_compress.bat
@@ -1,7 +1,7 @@
 for %%f in ("png_A\*.png") do optipng -o3 %%f
-for %%f in ("png_A\*.png") do optipng -o3 %%f
+for %%f in ("png_E\*.png") do optipng -o3 %%f
 for %%f in ("png_R\*.png") do optipng -o3 %%f
-for %%f in ("png_T\*.png") do optipng -o3 %%f
-for %%f in ("png_T2\*.png") do optipng -o3 %%f
-for %%f in ("png_T3\*.png") do optipng -o3 %%f
+for %%f in ("png_TL\*.png") do optipng -o3 %%f
+for %%f in ("png_TR\*.png") do optipng -o3 %%f
+for %%f in ("png_T10000\*.png") do optipng -o3 %%f
 pause
